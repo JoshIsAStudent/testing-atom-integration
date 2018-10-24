c51535ee5801f7e81243c646730427a0d3bf3d54
Joshua <JOSHUA.PONTE18@bathspa.ac.uk>
2018-10-24 11:58:40 +0100

    C++ program created

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+// C++ program to test version control with GitHub via Atom, and to test the g++ compiler
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  cout << "Hello World!"
+  return 0;
+}
